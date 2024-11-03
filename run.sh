4475e07522f35d0efb4a89563059251079059866
Your Name <therappha@gmail.com>
2024-11-03 04:28:52 +0000

    added fi

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
-
+make re
 if find . -name "libft.a" | grep -q "libft.a"; then
     echo -e "${GREEN}libft.a built! Executing tests...${RESET}"
     echo "Cloning 'therappha's libftester'"
