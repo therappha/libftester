25c17b17ab49adb2eb87d6a950a87f2014b89ca7
Your Name <therappha@gmail.com>
2024-11-03 04:24:49 +0000

    libftester !

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
-
+# Define color variables for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-echo -e "${GREEN}Making file...${RESET}"
-make re
-
 if find . -name "libft.a" | grep -q "libft.a"; then
     echo -e "${GREEN}libft.a built! Executing tests...${RESET}"
-    ./libftester/make
+    echo "Cloning 'therappha's libftester'"
+    git clone https://github.com/therappha/libftester.git
+    cd libftester
+    echo "bulding tests..."
+    ./make
+    ./tests
+    cd ..
+    echo "Deleting libftester!..."
+    rm -rf libftester
+    make fclean
 else
     echo -e "${RED}Error: libft.a not found!${RESET}"
 fi
